c753194d216d214319957b4ce1266d62f2ee58f5
Zayd Abdullayev <zeyyadabuzayd@gmail.com>
2024-09-16 04:33:01 -0700

    update for sidebar options

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,43 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import terser from "@rollup/plugin-terser";
-import builtins from "rollup-plugin-node-builtins";
-import globals from "rollup-plugin-node-globals";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import alias from "@rollup/plugin-alias";
 import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.js",
   output: {
     file: "dist/bundle.js",
     format: "iife",
-    globals: {
-      process: "process",
-    },
+    sourcemap: true,
   },
   plugins: [
+    alias({
+      entries: [
+        { find: "react", replacement: "preact/compat" },
+        { find: "react-dom", replacement: "preact/compat" },
+      ],
+    }),
     resolve({
       browser: true,
-      preferBuiltins: false,
+      extensions: [".js", ".jsx"],
     }),
     commonjs(),
-    builtins(),
-    globals(),
-    terser({
-      mangle: {
-        properties: true,
-      },
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      preventAssignment: true,
+    }),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-react"],
+      babelHelpers: "bundled",
     }),
     postcss({
-      plugins: [
-        require("cssnano")({
-          preset: "default",
-        }),
-      ],
-      // Extract CSS to the same location as JS file
       extract: true,
-      // Minify CSS output
       minimize: true,
     }),
+    terser(),
   ],
 };
